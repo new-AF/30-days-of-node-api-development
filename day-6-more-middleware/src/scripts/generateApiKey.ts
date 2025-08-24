736cdbcd4d4880e0c72934b461901cc8dc68e573
Abdullah Fatota <53046436+new-AF@users.noreply.github.com>
2025-08-24 16:35:15 +0200

    update day 6

--- day-6-more-middleware/src/scripts/generateApiKey.ts
+++ day-6-more-middleware/src/scripts/generateApiKey.ts
@@ -1,6 +1,6 @@
 import crypto from "crypto";
 
-// 32 random bytes, 128 length string
+// 32 random bytes, 64 length string
 const apiKey = crypto.randomBytes(32).toString("hex");
 
 console.log(apiKey);
