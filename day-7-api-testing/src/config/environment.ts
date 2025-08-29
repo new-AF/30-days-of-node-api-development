753b4d05631d15580b34a10124ea7f6a35271dba
Abdullah Fatota <53046436+new-AF@users.noreply.github.com>
2025-08-29 15:10:15 +0200

    update day 6 7

--- day-7-api-testing/src/config/environment.ts
+++ day-7-api-testing/src/config/environment.ts
@@ -1,10 +1,22 @@
 import dotenv from "dotenv";
 import path from "path";
+import { fileURLToPath } from "url";
+
+// Recreate __dirname in ESM
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Get current project folder name (e.g. "day-x-...")
+const currentFolder = path.basename(path.resolve(__dirname, "../../"));
+
+const absolutePath = path.resolve(process.cwd(), currentFolder + "/.env");
 
 // Load the .env from the current day's folder
 dotenv.config({
-    path: path.resolve(process.cwd(), "day-7-api-testing/.env"),
+    path: absolutePath,
 });
+
+// console.log({ absolutePath, currentFolder });
 
 // Fallback to port 3000 if not defined
 export const PORT: number = parseInt(process.env.PORT || "3000", 10);
